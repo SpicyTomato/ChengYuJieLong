27801ae739336ad882d1fe6f406b559023ce3228
0四宫辉夜0 <2940330764@qq.com>
2020-09-27 22:11:11 +0800

    Initial commit

---
+++ app/src/main/java/com/spicytomato/chengyujielong/Judge.java
@@ -0,0 +1,163 @@
+package com.spicytomato.chengyujielong;
+
+import java.util.Random;
+
+public class Judge {
+
+    public final static boolean HORIZONTAL = true;
+    public final static boolean VERTICAL = false;
+
+
+    public static int selectNumber(int width, int height, boolean preOrientation, int preX, int preY, String[][] words) {
+        int y_top = 0;
+        int x_left = 0;
+        int y_bottom = 0;
+        int x_right = 0;
+        int result = -1;
+        int length = 0;
+
+
+        if (preOrientation == HORIZONTAL) {
+            for (int i = preY - 1; i >= 0; i--) {
+                if (words[preX][i] == null) {
+                    x_left++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            for (int i = preY + 1; i <= width - 1; i++) {
+                if (words[preX][i] == null) {
+                    x_right++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            length = x_left + x_right;
+            Random random = new Random();
+            if (length == 3) {
+
+                switch (x_left) {
+                    case 0:
+                        result = 1;
+                        break;
+                    case 1:
+                        result = 2;
+                        break;
+                    case 2:
+                        result = 3;
+                        break;
+                    default:
+                        result = -1;
+                }
+            } else if (length > 3) {
+                int rest = length - 3;
+                switch (x_left) {
+                    case 0:
+                        result = 1;
+                        break;
+                    case 1:
+                        result = random.nextInt(2) + 1;
+                        break;
+                    case 2:
+                        if (rest > 1) {
+                            result = random.nextInt(3) + 1;
+                        } else {
+                            result = 1 + random.nextInt(2) + 1;
+                        }
+                        break;
+                    default:
+                        if (x_right == 0) {
+                            result = 4;
+                        }
+                        if (x_right == 1) {
+                            result = 2 + random.nextInt(2) + 1;
+                        }
+                        if (x_right == 2) {
+                            result = 1 + random.nextInt(3) + 1;
+                        }
+                        if (x_right >= 3) {
+                            result = random.nextInt(4) + 1;
+                        }
+                        break;
+                }
+            }
+
+            return result;
+
+        }
+
+        if (preOrientation == VERTICAL) {
+            for (int i = preX - 1; i >= 0; i--) {
+                if (words[i][preY] == null) {
+                    y_top++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            for (int i = preY + 1; i <= height - 1; i++) {
+                if (words[i][preY] == null) {
+                    y_bottom++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            length = y_top + y_bottom;
+            Random random = new Random();
+            if (length == 3) {
+
+                switch (y_top) {
+                    case 0:
+                        result = 1;
+                        break;
+                    case 1:
+                        result = 2;
+                        break;
+                    case 2:
+                        result = 3;
+                        break;
+                    default:
+                        result = -1;
+                }
+            } else if (length > 3) {
+                int rest = length - 3;
+                switch (y_top) {
+                    case 0:
+                        result = 1;
+                        break;
+                    case 1:
+                        result = random.nextInt(2) + 1;
+                        break;
+                    case 2:
+                        if (rest > 1) {
+                            result = random.nextInt(3) + 1;
+                        } else {
+                            result = 1 + random.nextInt(2) + 1;
+                        }
+                        break;
+                    default:
+                        if (y_bottom == 0) {
+                            result = 4;
+                        }
+                        if (y_bottom == 1) {
+                            result = 2 + random.nextInt(2) + 1;
+                        }
+                        if (y_bottom == 2) {
+                            result = 1 + random.nextInt(3) + 1;
+                        }
+                        if (y_bottom >= 3) {
+                            result = random.nextInt(4) + 1;
+                        }
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        return -1;
+    }
+}
---
+++ app/src/main/java/com/spicytomato/chengyujielong/MainActivity.java
@@ -0,0 +1,55 @@
+package com.spicytomato.chengyujielong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[][] mWords;
+    private boolean[][] mHasExisted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mWords = new String[12][9];
+        mHasExisted = new boolean[12][9];
+
+        for (int i = 4; i <= 7; i++) {
+            mWords[4][i] = "1";
+        }
+
+
+        int result = Judge.selectNumber(12, 9, Judge.HORIZONTAL, 4, 4, mWords);
+
+
+//        generate(4, 4);
+    }
+
+    public void generate(int x, int y, boolean orientation) {
+        mHasExisted[x][y] = true;
+        int result = Judge.selectNumber(12, 9, orientation, x, y, mWords);
+        int[] cross = paint(result, !orientation);
+        generate(cross[0], cross[1], !orientation);
+//        if (!orientation == Judge.HORIZONTAL) {
+//            if (mHasExisted[cross[0]][cross[1] + 1] && mWords[cross[0]][cross[1] + 1] != null) {
+//                generate(cross[0], cross[1] + 1 ,!orientation);
+//            }
+//        }
+    }
+
+    /**
+     * @param result
+     * @param orientation
+     * @return 返回生成下一个成语的位置
+     */
+
+    public int[] paint(int result, boolean orientation) {
+
+    }
+
+
+}
